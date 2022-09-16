269a71c1b294a1f4a8464959287230f8848cf4b0
heinrichhaenn <chunxuhan007@gmail.com>
2022-09-16 16:56:35 +0800

    time estimation

--- gset_test_1k_1core.R
+++ gset_test_1k_1core.R
@@ -1,3 +1,6 @@
+## This is a R script used to test the feasiblity and estimate the running time ##
+## We use 1000 sampels (sample names are stored in sample_1k.csv) and apply 1 core of the PC ##
+
 # install and import packages
 library(BiocManager)
 library(IsoformSwitchAnalyzeR)
@@ -8,47 +11,55 @@ library(rhdf5)
 library(biomaRt)
 library(GSA)
 library(clusterProfiler)
+library(OmnipathR)
 options(stringsAsFactors = F) 
 
-setwd('/home/projects/kvs_ccc/')
-ARCHS4_transcript_file = "/home/databases/archs4/v11/human_transcript_v11_tpm.h5"
+start_time <- Sys.time()
+setwd('~/Desktop/CCI_DK/ccc/')
+dir.create('./test_output')
+ARCHS4_transcript_file = "~/Desktop/CCI_DK/ccc/human_transcript_v11_tpm.h5"
+sample_1k <- read.csv('~/Desktop/CCI_DK/ccc/sample_1k.csv',col.names = 'sample_name')$sample_name
+
+
+# Extract 1k samples from ARCHS4_transcript_file
 human_transcript_v11 <- H5Fopen(ARCHS4_transcript_file)
 h5dump(human_transcript_v11,load=FALSE)    # Dump the content of an HDF5 file
 samples <- human_transcript_v11$meta$samples$geo_accession
 transcripts <- human_transcript_v11$meta$transcripts$transcripts
-transcript_expression <- t(h5read(human_transcript_v11, "data/expression"))
+sample_locations = which(samples %in% sample_1k)
+transcript_expression <- t(h5read(human_transcript_v11, "data/expression", index=list(sample_locations, 1:length(transcripts))))
 H5close()
 rownames(transcript_expression) <- transcripts
-colnames(transcript_expression) <- samples
-write.csv(transcript_expression,'./transcript_expression.csv')
+colnames(transcript_expression) <- samples[sample_locations]
+write.csv(transcript_expression,'./test_output/transcript_expression.csv')
 print('The generation of transcripts expression matrix has been done!')
-saveRDS(transcript_expression,'./transcript_expression_matrix.rds')
+saveRDS(transcript_expression,'./test_output/transcript_expression_matrix.rds')
 
 # Quantification from transcripts to genes level
-### Import GTF file
-aSwitchList <- importGTF(pathToGTF='/home/databases/archs4/v11/Homo_sapiens.GRCh38.87.chr_patch_hapl_scaff.gtf')
+## Import GTF file
+aSwitchList <- importGTF(pathToGTF='./Homo_sapiens.GRCh38.87.chr_patch_hapl_scaff.gtf')
 
-### Create dataframe with associations
+## Create dataframe with associations
 isoGene <- unique(aSwitchList$isoformFeatures[c('isoform_id','gene_name')])
 colnames(isoGene) <- c('isoform_id','gene_id')
-
-### Overwrite with gene names
-#isoGene$gene_name <- aSwitchList$isoformFeatures$gene_name[match(
-#  isoGene$isoform_id, aSwitchList$isoformFeatures$isoform_id
-#)]
 
 gene_expression <- IsoformSwitchAnalyzeR::isoformToGeneExp(transcript_expression,
                                                            isoformGeneAnnotation = isoGene,
                                                            quiet = FALSE)
-write.csv(gene_expression,'./gene_expression.csv')
+write.csv(gene_expression,'./test_output/gene_expression.csv')
 print('The generation of genes expression matrix has been done!')
-saveRDS(gene_expression,'./gene_expression_matrix.rds')
+saveRDS(gene_expression,'./test_output/gene_expression_matrix.rds')
+
 
 # Quantification from genes to gene-sets level
 ## Import gene-sets from MSigDB (a combination of three gene sets -- CP + GO(BP+MF+CC) + Hallmarks)
-cp_gene_sets <- clusterProfiler::read.gmt('./data/c2.cp.v2022.1.Hs.symbols.gmt')
-h_gene_sets <- clusterProfiler::read.gmt('./data/h.all.v2022.1.Hs.symbols.gmt')
-go_gene_sets <- clusterProfiler::read.gmt('./data/c5.go.v2022.1.Hs.symbols.gmt')
+cp_gene_sets <- clusterProfiler::read.gmt('./c2.cp.v2022.1.Hs.symbols.gmt')
+h_gene_sets <- clusterProfiler::read.gmt('./h.all.v2022.1.Hs.symbols.gmt')
+#go_gene_sets <- clusterProfiler::read.gmt('c5.go.v2022.1.Hs.symbols.gmt')
+go_gene_sets <- OmnipathR::go_annot_slim(organism = 'human',
+                                         slim = 'agr',
+                                         aspects = c('C','F','P'),
+                                         cache = TRUE)
 gene_sets <- rbind(cp_gene_sets,h_gene_sets,go_gene_sets)
 gset_list <- split(gene_sets$gene, gene_sets$term)
 
@@ -59,9 +70,10 @@ gene_set_expression <- gsva(gene_expression_matrix,
                             method = 'ssgsea',
                             kcdf = 'Poisson',
                             verbose = T)
-write.csv(gene_set_expression,'./gset_matrix/gset_expression.csv')
+write.csv(gene_set_expression,'./gset_expression.csv')
 print('The generation of gene sets expression matrix has been done!')
-print('The gene sets expression matrix was strored in /home/projects/kvs_ccc//gset_matrix/gset_expression.csv')
 
+end_time <- Sys.time()
+print('The running time of this program is ',)
 
 
