4bb1d2d679418425391a5623f8edcabe13ebdfa7
heinrichhaenn <chunxuhan007@gmail.com>
2022-09-27 23:20:49 +0800

    run on PC

--- correlation_matrix.R
+++ correlation_matrix.R
@@ -1,9 +1,13 @@
 ## The Rscript run on my Macbook to calculate the correlation matrix ##
 ## Subset a small matrix to estimate the time cost ##
+## But it is so slow, so I'm thinking about trying something new ##
+## Btw the time cost: 1.3h for only 10 samples (10/16000+) and 30 gene sets (30/3152) ##
+
 
 library(Hmisc)
 library(OmnipathR)
 library(dplyr)
+library(stringr)
 
 setwd('/home/projects/kvs_ccc/')
 # Generation of correlation matrix
@@ -18,19 +22,71 @@ dplyr::select(category_intercell_source,source,source_genesymbol,
 ligand <- unique(ligand_receptor$source_genesymbol)
 receptor <- unique(ligand_receptor$target_genesymbol)
 
-## Establish
-cor_matrix <- matrix(nrow = 3152,ncol = 1078,dimnames = list(rownames(gene_set_expression),c(ligand,receptor)))
+## Establish the spearman correlation matrix
+cor_matrix <- matrix(nrow = nrow(gene_set_expression),ncol = length(ligand) + length(receptor),
+                     dimnames = list(rownames(gene_set_expression),c(ligand,receptor)))
+p_matrix <- matrix(nrow = nrow(gene_set_expression),ncol = length(ligand) + length(receptor),
+                   dimnames = list(rownames(gene_set_expression),c(ligand,receptor)))
 pb <- txtProgressBar(style=3)
+print('Begin!')
 star_time <- Sys.time()
+loop <- 0
 for (i in 1:nrow(cor_matrix)){
   for (j in 1:ncol(cor_matrix)){
+    lr_symbol <- colnames(cor_matrix)[j]
+    gene_set_symbol <- rownames(cor_matrix)[i]
+    if (str_detect(lr_symbol,'_')){
+      subunit <- unlist(strsplit(lr_symbol,split = '_'))
+      if (all(subunit %in% rownames(gene_expression))){
+        lr_score <- complex_expression(subunit)
+      } else {
+        next
+      }
+    }
+    else {
+      if (lr_symbol %in% rownames(gene_expression)){
+        lr_score <-gene_expression[lr_symbol,]
+      } else {
+        next
+      }
+    }
     gene_set_score <- gene_set_expression[rownames(cor_matrix)[i],]
-    lr_score <- gene_expression[colnames(cor_matrix)[j],]
-    cor <- rcorr(gene_set_score,lr_score,type = 'spearman')
-    setTxtProgressBar(pb, i/(nrow(cor_matrix)+ncol(cor_matrix)))
+    cor_test <- cor.test(gene_set_score,lr_score,method = 'spearman',exact=FALSE)
+    cor <- cor_test$estimate
+    p <- cor_test$p.value
+    cor_matrix[i,j] <- cor
+    p_matrix[i,j] <- p
+    loop <- loop + 1
+    setTxtProgressBar(pb, loop/(30*ncol(cor_matrix)))
   }
+  print('A row is done!')
 }
 
+print('All is done!')
 end_time <- Sys.time()
 close(pb)
 run_time <- end_time - star_time
+
+### Calculation of geomatrix mean
+gm_mean = function(x, na.rm=TRUE, zero.propagate = FALSE){
+  if(any(x < 0, na.rm = TRUE)){
+    return(NaN)
+  }
+  if(zero.propagate){
+    if(any(x == 0, na.rm = TRUE)){
+      return(0)
+    }
+    exp(mean(log(x), na.rm = na.rm))
+  } else {
+    exp(sum(log(x[x > 0]), na.rm=na.rm) / length(x))
+  }
+}
+
+### For the ligand and receptor complex
+complex_expression <- function(subunit){
+  exp <- gene_expression[subunit,]
+  complex_exp <- apply(exp,2,gm_mean,na.rm=TRUE,zero.propagate = TRUE)
+  return(complex_exp)
+}
+
+
---
+++ correlation_matrix_pro.R
@@ -0,0 +1,46 @@
+cor_matrix_p <- matrix(nrow = nrow(gene_set_expression),ncol = length(ligand) + length(receptor),
+                     dimnames = list(rownames(gene_set_expression),c(ligand,receptor)))
+p_matrix_p <- matrix(nrow = nrow(gene_set_expression),ncol = length(ligand) + length(receptor),
+                   dimnames = list(rownames(gene_set_expression),c(ligand,receptor)))
+pb <- txtProgressBar(style=3)
+print('Begin!')
+star_time <- Sys.time()
+loop <- 0
+
+for (i in 1:ncol(cor_matrix)){
+  lr_symbol <- colnames(cor_matrix)[i]
+  if (str_detect(lr_symbol,'_')){
+    subunit <- unlist(strsplit(lr_symbol,split = '_'))
+    if (all(subunit %in% rownames(gene_expression))){
+      lr_score <- complex_expression(subunit)
+    } else {
+      loop <- loop + nrow(cor_matrix)
+      next
+    }
+  }
+  else {
+    if (lr_symbol %in% rownames(gene_expression)){
+      lr_score <-gene_expression[lr_symbol,]
+    } else {
+      loop <- loop + nrow(cor_matrix)
+      next
+    }
+  }
+  for (j in 1:nrow(cor_matrix)){
+    gene_set_symbol <- rownames(cor_matrix)[j]
+    gene_set_score <- gene_set_expression[gene_set_symbol,]
+    cor_test <- cor.test(gene_set_score,lr_score,method = 'spearman',exact=FALSE)
+    cor <- cor_test$estimate
+    p <- cor_test$p.value
+    cor_matrix_p[j,i] <- cor
+    p_matrix_p[j,i] <- p
+    loop <- loop + 1
+    setTxtProgressBar(pb, loop/(nrow(cor_matrix)*ncol(cor_matrix)))
+  }
+}
+
+print('All is done!')
+end_time <- Sys.time()
+close(pb)
+run_time <- end_time - star_time
+print(run_time)
