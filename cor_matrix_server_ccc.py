f3cb482cc1c48d573aea7f1affaf7b7d82ef6b92
heinrichhaenn <chunxuhan007@gmail.com>
2022-10-16 14:57:27 +0800

    update

--- cor_matrix_server_ccc.py
+++ cor_matrix_server_ccc.py
@@ -8,7 +8,7 @@ import math
 from scipy import stats
 
 os.chdir('/Users/kimhan/Desktop/CCI_DK/ccc')
-## Import the expression matrix
+# Import the expression matrix
 gene_expression_df = pd.read_csv('./gene_expression_matrix.csv', index_col=0)
 print(gene_expression_df.shape)
 gene_set_expression_df = pd.read_csv('./gene_set_expression_matrix.csv', index_col=0)
@@ -19,9 +19,12 @@ ligand = icn_df.loc[:, 'source_genesymbol'].drop_duplicates().values.tolist()
 print(len(ligand))
 receptor = icn_df.loc[:, 'target_genesymbol'].drop_duplicates().values.tolist()
 print(len(receptor))
+ligand_receptor = ligand + receptor
 
 test = [1, 2, 3, 0]
-def gm_mean(x, zero_propagate=False):
+
+
+def gm_mean(x, zero_propagate=True):
     if (np.array(x) < 0).any():
         return None
     if zero_propagate is True:
@@ -34,3 +37,29 @@ def gm_mean(x, zero_propagate=False):
         return g_mean
 
 
+def complex_expression(unit_list):
+    exp = gene_expression_df.loc[unit_list, :]
+    complex_exp = exp.apply(gm_mean, axis=0).tolist()
+    return complex_exp
+
+
+cor_df = pd.DataFrame(columns= ligand_receptor,index= gene_set_expression_df.index.values)
+p_df = pd.DataFrame(columns= ligand_receptor,index= gene_set_expression_df.index.values)
+loop = 0
+for lr_symbol in ligand_receptor:
+    if '_' in lr_symbol:
+        unit_list = lr_symbol.split(sep='_')
+        if set(unit_list).issubset(set(gene_expression_df.index.values)):
+            lr_score = complex_expression(unit_list)
+        else:
+            loop = loop + len(cor_df.index)
+            continue
+    else:
+        if lr_symbol in gene_expression_df.index:
+            lr_score = gene_expression_df.loc[lr_symbol]
+        else:
+            loop = loop + len(cor_df.index)
+            continue
+    for gene_set_symbol in gene_set_expression_df.index.values:
+        gene_set_score = gene_set_expression_df.loc[gene_set_symbol]
+        ccc(lr_score,gene_set_score)
